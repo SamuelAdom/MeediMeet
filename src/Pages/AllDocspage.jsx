728ab73123348a8a1640efbc91a71bbe370b4491
SamuelAdom <samueldebrahmireku7@gmail.com>
2025-05-26 11:39:16 +0000

    final commit

--- src/Pages/AllDocspage.jsx
+++ src/Pages/AllDocspage.jsx
@@ -31,21 +31,22 @@ applyFilter()
 
 
   return (
-    <div className="m-10 flex flex-row">
+    <div className="m-3 sm:m-10 flex flex-col sm:flex-row">
+      
       <div className="flex flex-col gap-2 items-start">
-        <h1 className="text-sm md:text-2xl">Browse through the doctors specialist.</h1>
-        <button>Filter</button>
-        <div className='flex flex-col gap-2.3'>
-            <p onClick={()=>speciality==="General physician"? navigate("/all-doctors"): navigate("/all-doctors/General physician")} className={`text-sm md:text-md cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md w-40 ${speciality==="General physician"? "bg-gray-400 text-black":""}`}>General physician</p>
-        <p onClick={()=>speciality==="Gynecologist"? navigate("/all-doctors"): navigate("/all-doctors/Gynecologist")} className={`text-sm md:text-md cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md w-40 ${speciality==="Gynecologist"? "bg-gray-400 text-black":""}`}>Gynecologist</p>
-        <p onClick={()=>speciality==="Dermatologist"? navigate("/all-doctors"): navigate("/all-doctors/Dermatologist")} className={`text-sm md:text-md cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md w-40 ${speciality==="Dermatologist"? "bg-gray-400 text-black":""}`}>Dermatologist</p>
-        <p onClick={()=>speciality==="Pediatricians"? navigate("/all-doctors"): navigate("/all-doctors/Pediatricians")} className={`text-sm md:text-md cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md w-40 ${speciality==="Pediatricians"? "bg-gray-400 text-black":""}`}>Pediatricians</p>
-        <p onClick={()=>speciality==="Neurologist"? navigate("/all-doctors"): navigate("/all-doctors/Neurologist")} className={`text-sm md:text-md cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md w-40 ${speciality==="Neurologist"? "bg-gray-400 text-black":""}`}>Neurologist</p>
-        <p onClick={()=>speciality==="Gastroenterologist"? navigate("/all-doctors"): navigate("/all-doctors/Gastroenterologist")} className={`text-sm md:text-md  cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md w-40 ${speciality==="Gastroenterologist"? "bg-gray-400 text-black":""}`}>Gastroenterologist</p>
+        <h1 className="text-lg sm:text-2xl">Browse through the doctors specialist.</h1>
+        <button onClick={()=>setShowFilter(prev=>!prev)} className={`py-1.5 px-4 rounded-sm border transition-all sm:hidden ${showFilter?"bg-blue-400 text-white text-sm":""}`}>Filter</button>
+        <div className={`flex-col gap-2.5 w-full sm:w-40 ${showFilter?"flex":"hidden sm:flex" }`}>
+            <p onClick={()=>speciality==="General physician"? navigate("/all-doctors"): navigate("/all-doctors/General physician")} className={`text-sm md:text-md cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md ${speciality==="General physician"? "bg-gray-400 text-black":""}`}>General physician</p>
+        <p onClick={()=>speciality==="Gynecologist"? navigate("/all-doctors"): navigate("/all-doctors/Gynecologist")} className={`text-sm md:text-md cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md  ${speciality==="Gynecologist"? "bg-gray-400 text-black":""}`}>Gynecologist</p>
+        <p onClick={()=>speciality==="Dermatologist"? navigate("/all-doctors"): navigate("/all-doctors/Dermatologist")} className={`text-sm md:text-md cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md  ${speciality==="Dermatologist"? "bg-gray-400 text-black":""}`}>Dermatologist</p>
+        <p onClick={()=>speciality==="Pediatricians"? navigate("/all-doctors"): navigate("/all-doctors/Pediatricians")} className={`text-sm md:text-md cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md  ${speciality==="Pediatricians"? "bg-gray-400 text-black":""}`}>Pediatricians</p>
+        <p onClick={()=>speciality==="Neurologist"? navigate("/all-doctors"): navigate("/all-doctors/Neurologist")} className={`text-sm md:text-md cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md  ${speciality==="Neurologist"? "bg-gray-400 text-black":""}`}>Neurologist</p>
+        <p onClick={()=>speciality==="Gastroenterologist"? navigate("/all-doctors"): navigate("/all-doctors/Gastroenterologist")} className={`text-sm md:text-md  cursor-pointer border-1 border-green-300 text-gray-800 py-1.5 px-4 rounded-md ${speciality==="Gastroenterologist"? "bg-gray-400 text-black":""}`}>Gastroenterologist</p>
         </div>
       
       </div>
-      <div className="grid grid-cols-1 ml-10 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 sm:ml-10 md:grid-cols-2 lg:grid-cols-3 gap-2.5">
         {filter.map((item)=>(
           <FilterCard key={item.id} item={item}/>
         
